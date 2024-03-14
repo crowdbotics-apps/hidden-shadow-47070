3eb1823e274a97112baecf69369e7b46e6045736
Crowdbotics <team@crowdbotics.com>
2024-03-14 23:42:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-shadow-47070",
+  url: "https://hidden-shadow-47070.botics.co",
+  api: "https://hidden-shadow-47070.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
